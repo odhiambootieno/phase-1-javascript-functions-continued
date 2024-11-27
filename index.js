719b20b535d0b87c5e626130039b131ceac16c02
odhiambootieno <brianmkcnight@gmail.com>
2024-11-27 20:41:35 +0300

    adds

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+// Function to define Saturday fun activities
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function to define Monday work activities
+  const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Function to wrap an adjective with visual flair
+  function wrapAdjective(flair = '*') {
+    return function(adjective = 'special') {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
